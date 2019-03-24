cd1369c2a18f7b94773e5e36cbbb64e2843db6f8
Ruslan Bednyakov <ruslanbednyakov90@gmail.com>
2019-03-25 02:34:26 +0300

    add simple people test

--- src/ducks/people.js
+++ src/ducks/people.js
@@ -1,5 +1,7 @@
 import {appName} from '../config'
 import {Record, List} from 'immutable'
+import {put, call, takeEvery} from 'redux-saga/effects'
+import {generateId} from './utils'
 
 const ReducerRecord = Record({
   entities: new List([])
@@ -13,6 +15,7 @@ const PersonRecord = Record({
 })
 
 export const moduleName = 'people'
+export const ADD_PERSON_REQUEST = `${appName}/${moduleName}/ADD_PERSON_REQUEST`
 export const ADD_PERSON = `${appName}/${moduleName}/ADD_PERSON`
 
 export default function reducer(state = new ReducerRecord(), action) {
@@ -20,7 +23,7 @@ export default function reducer(state = new ReducerRecord(), action) {
 
   switch (type) {
     case ADD_PERSON:
-      return state.set('entities', entities => entities.push(new PersonRecord(payload.person)));
+      return state.set('entities', entities => entities.push(new PersonRecord(payload)));
 
     default: 
       return state;
@@ -28,12 +31,20 @@ export default function reducer(state = new ReducerRecord(), action) {
 }
 
 export function addPerson(person) {
-  return (dispatch) => {
-    dispatch({
-      type: ADD_PERSON,
-      payload: {
-        person: {id: Date.now(), ...person}
-      }
-    })
+  return {
+    type: ADD_PERSON_REQUEST,
+    payload: person
   }
+}
+
+export const addPersonSaga = function * (action) {
+  const id = yield call(generateId);
+  yield put({
+    type: ADD_PERSON,
+    payload: {...action.payload, id}
+  })
+}
+
+export const saga = function * () {
+  yield takeEvery(ADD_PERSON_REQUEST, addPersonSaga)
 }
--- src/redux/index.js
+++ src/redux/index.js
@@ -3,10 +3,18 @@ import reducer from './reducer'
 import logger from 'redux-logger'
 import thunk from 'redux-thunk'
 import {routerMiddleware} from 'react-router-redux'
+import createSagaMiddleware from 'redux-saga'
 import history from '../history'
+import {saga} from '../ducks/people'
 
-const enchancer = applyMiddleware(routerMiddleware(history), thunk, logger)
+const sagaMiddleware = createSagaMiddleware();
+const enchancer = applyMiddleware(sagaMiddleware, routerMiddleware(history), thunk, logger);
 
-const store = createStore(reducer, enchancer)
+const store = createStore(reducer, enchancer);
+
+// For development only
+window.store = store;
+
+sagaMiddleware.run(saga);
 
 export default store
