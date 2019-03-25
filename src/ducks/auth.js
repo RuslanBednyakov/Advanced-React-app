3ee920b0d0ed436124e81a5a051dbad3e817fc36
Ruslan Bednyakov <ruslanbednyakov90@gmail.com>
2019-03-25 03:08:12 +0300

    add signup saga

--- src/ducks/auth.js
+++ src/ducks/auth.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase'
 import {appName} from '../config'
 import {Record} from 'immutable'
+import {all, take, call, put} from 'redux-saga/effects'
 
 const ReducerRecord = Record({
   user: null,
@@ -12,6 +13,7 @@ export const moduleName = 'auth'
 export const SIGN_UP_REQUEST = `${appName}/${moduleName}/SIGN_UP_REQUEST`
 export const SIGN_UP_SUCCESS = `${appName}/${moduleName}/SIGN_UP_SUCCESS`
 export const SIGN_UP_ERROR = `${appName}/${moduleName}/SIGN_UP_ERROR`
+export const SIGN_IN_SUCCESS = `${appName}/${moduleName}/SIGN_IN_SUCCESS`
 
 export default function reducer(state = new ReducerRecord(), action) {
   const {type, payload, error} = action;
@@ -37,19 +39,38 @@ export default function reducer(state = new ReducerRecord(), action) {
 }
 
 export function signUp(email, password) {
-  return (dispatch) => {
-    dispatch({
-      type: SIGN_UP_REQUEST
-    })
+  return {
+      type: SIGN_UP_REQUEST,
+      payload: {email, password}
+    }
+}
 
-    firebase.auth().createUserWithEmailAndPassword(email, password)
-      .then(user => dispatch({
+export const signUpSaga = function * () {
+  const auth = firebase.auth();
+
+  while(true) {
+    const action = yield take(SIGN_UP_REQUEST);
+
+    try{
+      const user = yield call(
+        [auth, auth.createUserWithEmailAndPassword],
+        action.payload.email, action.payload.password
+      )
+      yield put({
         type: SIGN_UP_SUCCESS,
         payload: {user}
-      }))
-      .catch(error => dispatch({
+      })
+    } catch (error) {
+      yield put({
         type: SIGN_UP_ERROR,
         error
-      }))
+      })
+    }
   }
+}
+
+export const saga = function * () {
+  yield all([
+    signUpSaga()
+  ])
 }
--- src/redux/index.js
+++ src/redux/index.js
@@ -5,7 +5,7 @@ import thunk from 'redux-thunk'
 import {routerMiddleware} from 'react-router-redux'
 import createSagaMiddleware from 'redux-saga'
 import history from '../history'
-import {saga} from '../ducks/people'
+import rootSaga from './saga'
 
 const sagaMiddleware = createSagaMiddleware();
 const enchancer = applyMiddleware(sagaMiddleware, routerMiddleware(history), thunk, logger);
@@ -15,6 +15,6 @@ const store = createStore(reducer, enchancer);
 // For development only
 window.store = store;
 
-sagaMiddleware.run(saga);
+sagaMiddleware.run(rootSaga);
 
 export default store
