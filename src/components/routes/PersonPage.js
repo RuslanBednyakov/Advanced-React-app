24a0bc3692c1a257202a5801194f931db476c665
Ruslan Bednyakov <ruslanbednyakov90@gmail.com>
2019-04-01 23:11:53 +0300

    add people func

--- src/components/routes/PersonPage.js
+++ src/components/routes/PersonPage.js
@@ -1,18 +1,27 @@
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
-import {addPerson} from '../../ducks/people'
-import NewPersonForm from '../people/NewPersonForm';
-
+import React, { Component } from 'react'
+import {connect} from 'react-redux'
+import {addPerson, moduleName} from '../../ducks/people'
+import NewPersonForm from '../people/NewPersonForm'
+import PeopleTable from '../people/PeopleTable'
+import Loader from '../common/Loader'
 
 class PersonPage extends Component {
   render() {
+    const {loading, addPerson} = this.props
+
     return (
       <div>
         <h2>Add new person</h2>
-        <NewPersonForm onSubmit={this.props.addPerson} />
+        <PeopleTable />
+        {loading
+          ? <Loader />
+          : <NewPersonForm onSubmit={addPerson} />
+        }
       </div>
     );
   }
 }
 
-export default connect(null, {addPerson})(PersonPage);
+export default connect(state => ({
+  loading: state[moduleName].loading
+}), {addPerson})(PersonPage);
--- src/ducks/people.js
+++ src/ducks/people.js
@@ -1,50 +1,121 @@
 import {appName} from '../config'
-import {Record, List} from 'immutable'
-import {put, call, takeEvery} from 'redux-saga/effects'
-import {generateId} from './utils'
+import {Record, OrderedMap} from 'immutable'
+import {put, call, takeEvery, all} from 'redux-saga/effects'
+import {fbDataToEntities} from './utils'
+import firebase from 'firebase'
+import {createSelector} from 'reselect'
+import {reset} from 'redux-form'
 
-const ReducerRecord = Record({
-  entities: new List([])
+const ReducerState = Record({
+  entities: new OrderedMap({}),
+  loading: false
 })
 
 const PersonRecord = Record({
-  id: null,
+  uid: null,
   firstName: null,
   lastName: null,
   email: null
 })
 
 export const moduleName = 'people'
-export const ADD_PERSON_REQUEST = `${appName}/${moduleName}/ADD_PERSON_REQUEST`
-export const ADD_PERSON = `${appName}/${moduleName}/ADD_PERSON`
+const prefix = `${appName}/${moduleName}`
 
-export default function reducer(state = new ReducerRecord(), action) {
-  const {type, payload} = action;
+export const ADD_PERSON_REQUEST = `${prefix}/ADD_PERSON_REQUEST`
+export const ADD_PERSON_SUCCESS = `${prefix}/ADD_PERSON_SUCCESS`
+export const ADD_PERSON_ERROR = `${prefix}/ADD_PERSON_ERROR`
+export const FETCH_ALL_REQUEST = `${prefix}/FETCH_ALL_REQUEST`
+export const FETCH_ALL_SUCCESS = `${prefix}/FETCH_ALL_SUCCESS`
+export const FETCH_ALL_ERROR = `${prefix}/FETCH_ALL_ERROR`
+
+export default function reducer(state = new ReducerState(), action) {
+  console.log('reducer')
+  const {type, payload} = action
 
   switch (type) {
-    case ADD_PERSON:
-      return state.set('entities', entities => entities.push(new PersonRecord(payload)));
+    case FETCH_ALL_REQUEST:
+    case ADD_PERSON_REQUEST:
+      return state.set('loading', true)
 
-    default: 
-      return state;
+    case ADD_PERSON_SUCCESS:
+      return state
+        .set('loading', false)
+        .setIn(['entities', payload.uid], new PersonRecord(payload))
+
+    case FETCH_ALL_SUCCESS:
+      return state
+        .set('loading', false)
+        .set('entities', fbDataToEntities(payload, PersonRecord))
+
+    default:
+      return state
   }
 }
 
+export const stateSelector = state => state[moduleName]
+export const entitiesSelector = createSelector(stateSelector, state => state.entities)
+export const peopleListSelector = createSelector(entitiesSelector, entities => entities.valueSeq().toArray())
+
 export function addPerson(person) {
+  console.log('action')
   return {
     type: ADD_PERSON_REQUEST,
     payload: person
   }
 }
 
+export function fetchAllPeople() {
+  console.log('action')
+  return {
+    type: FETCH_ALL_REQUEST
+  }
+}
+
 export const addPersonSaga = function * (action) {
-  const id = yield call(generateId);
-  yield put({
-    type: ADD_PERSON,
-    payload: {...action.payload, id}
-  })
+  console.log('saga')
+  const peopleRef = firebase.database().ref('people')
+
+  try {
+    const ref = yield call([peopleRef, peopleRef.push], action.payload)
+
+    yield put({
+        type: ADD_PERSON_SUCCESS,
+        payload: {...action.payload, uid: ref.key}
+    })
+
+    yield put(reset('person'))
+
+  } catch (error) {
+    yield put({
+      type: ADD_PERSON_ERROR,
+      error
+    })
+  }
+}
+
+export const fetchAllSaga = function * () {
+  console.log('saga')
+  const peopleRef = firebase.database().ref('people')
+
+  try {
+    const data = yield call([peopleRef, peopleRef.once], 'value')
+
+    yield put({
+      type: FETCH_ALL_SUCCESS,
+      payload: data.val()
+    })
+  } catch (error) {
+    yield put({
+      type: FETCH_ALL_ERROR,
+      error
+    })
+  }
 }
 
 export const saga = function * () {
-  yield takeEvery(ADD_PERSON_REQUEST, addPersonSaga)
+  console.log('saga-all')
+  yield all([
+    takeEvery(ADD_PERSON_REQUEST, addPersonSaga),
+    takeEvery(FETCH_ALL_REQUEST, fetchAllSaga),
+  ])
 }
